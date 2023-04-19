1ccfce8b49243309e09da5d377d71cf0ffebaeff
enyaaad <enindima1@gmail.com>
2023-04-19 18:11:46 +0700

    [update] add element+dragndrop

--- src/app/pages/constructor/constructor.component.ts
+++ src/app/pages/constructor/constructor.component.ts
@@ -1,48 +1,89 @@
-import {Component, ViewChild} from '@angular/core';
-import {Observable, of} from "rxjs";
-import {KonvaComponent} from "ng2-konva";
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
-declare const Konva: any;
-let ng: any;
+interface Element {
+  x: number;
+  y: number;
+  size: number;
+  color: string;
+}
 @Component({
   selector: 'app-constructor',
   templateUrl: './constructor.component.html',
   styleUrls: ['./constructor.component.sass']
 })
-export class ConstructorComponent {
-  @ViewChild('stage') stage!: KonvaComponent;
-  @ViewChild('layer') layer!: KonvaComponent;
-  @ViewChild('dragLayer') dragLayer!: KonvaComponent;
-  public handleDragend(ngComponent: KonvaComponent) {
-    const shape = ngComponent.getStage();
-    const layer = ng.layer.getStage();
-    const stage = ng.stage.getStage();
-
-    shape.moveTo(layer);
-    stage.draw();
-
-    shape.to({
-      duration: 0.5,
-      easing: Konva.Easings.ElasticEaseOut,
-      scaleX: ngComponent.getConfig().startScale,
-      scaleY: ngComponent.getConfig().startScale,
-      shadowOffsetX: 5,
-      shadowOffsetY: 5
-    });
+export class ConstructorComponent implements OnInit{
+  ngOnInit() {
   }
-  public configStage: Observable<any> = of({
-    width: 800,
-    height: 400
-  });
-  public configCircle: Observable<any> = of({
-    x: 200,
-    y: 100,
-    radius: 70,
-    fill: "red",
-    stroke: "black",
-    strokeWidth: 4
-  });
-  start():void{
 
+  @ViewChild('canvas') canvasRef!: ElementRef<HTMLCanvasElement>;
+  canvasWidth = 800;
+  canvasHeight = 600;
+  elements: Element[] = [];
+  draggingElementIndex: number | null = null;
+  dragOffsetX = 0;
+  dragOffsetY = 0;
+
+
+  handleMouseDown(event: MouseEvent) {
+    const canvas = this.canvasRef.nativeElement;
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    this.draggingElementIndex = this.elements.findIndex(
+      (element) =>
+        x >= element.x &&
+        x <= element.x + element.size &&
+        y >= element.y &&
+        y <= element.y + element.size
+    );
+
+    if (this.draggingElementIndex !== -1) {
+      this.dragOffsetX = x - this.elements[this.draggingElementIndex].x;
+      this.dragOffsetY = y - this.elements[this.draggingElementIndex].y;
+    } else {
+      const newElement: Element = {
+        x,
+        y,
+        size: 50,
+        color: 'red'
+      };
+      this.elements.push(newElement);
+      this.draggingElementIndex = this.elements.indexOf(newElement);
+      this.dragOffsetX = 0;
+      this.dragOffsetY = 0;
+    }
+  }
+
+  handleMouseMove(event: MouseEvent) {
+    if (this.draggingElementIndex !== null) {
+      const canvas = this.canvasRef.nativeElement;
+      const rect = canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left - this.dragOffsetX;
+      const y = event.clientY - rect.top - this.dragOffsetY;
+
+      this.elements[this.draggingElementIndex].x = x;
+      this.elements[this.draggingElementIndex].y = y;
+
+      this.drawElements();
+    }
+  }
+
+  handleMouseUp(event: MouseEvent) {
+    this.draggingElementIndex = null;
+    this.dragOffsetX = 0;
+    this.dragOffsetY = 0;
+  }
+
+  drawElements() {
+    const canvas = this.canvasRef.nativeElement;
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+      this.elements.forEach((element) => {
+        ctx.fillStyle = element.color;
+        ctx.fillRect(element.x, element.y, element.size, element.size);
+      });
+    }
   }
 }
